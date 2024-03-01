6902b4915fd10d5ab436d710f6c3986f4833a032
dehemi-madara <dehemimadara@gmail.com>
2024-03-01 09:44:38 +0530

    updated

--- script.js
+++ script.js
@@ -59,3 +59,28 @@ circles.forEach(elem => {
     }
 });
 
+//mix it up portfolio section
+var mixer = mixitup('.portfolio-gallery');
+
+
+// circle skill ////////////////////////////////////////////////
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+
+// sticky navbar ////////////////////////////////////////////////
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY > 50)
+})
